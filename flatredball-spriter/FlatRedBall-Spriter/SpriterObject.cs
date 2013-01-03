ee69e75ef2fb0e214ce9e4f1e64b182ed2bd4e3d
kainazzzo <ddatti@gmail.com>
2013-01-03 00:13:54 -0500

    Moving along

--- flatredball-spriter/FlatRedBall-Spriter/SpriterObject.cs
+++ flatredball-spriter/FlatRedBall-Spriter/SpriterObject.cs
@@ -3,17 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using FlatRedBall;
+using FlatRedBall.Instructions;
 using FlatRedBall.Math;
 
 namespace FlatRedBall_Spriter
 {
     public class SpriterObject : PositionedObject
     {
-        public SpriterObject(PositionedObjectList<Sprite> spriteList)
+        public SpriterObject()
         {
-            SpriteList = spriteList;
+            SpriteList = new PositionedObjectList<Sprite>();
         }
 
-        public PositionedObjectList<Sprite> SpriteList { get; private set; } 
+        public PositionedObjectList<Sprite> SpriteList { get; private set; }
     }
 }

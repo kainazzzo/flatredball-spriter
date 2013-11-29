3569b81748c224f89d21bcd2290573f8a5efe3b9
Domenic Datti <ddatti@outlook.com>
2013-11-29 13:20:58 -0800

    Added a new project for frb extensions and a ScaledPositionedObject

---
+++ flatredball-extensions-tests/ScaledPositionedObjectTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FlatRedBall;
+using FlatRedBallExtensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Xna.Framework;
+
+namespace flatredball_extensions_tests
+{
+    [TestClass]
+    public class ScaledPositionedObjectTests
+    {
+        [TestMethod]
+        public void RelativePositionAffectedByParentScale()
+        {
+            var spo = new ScaledPositionedObject { Position = new Vector3(100, 200, 300) };
+            var parent = new ScaledPositionedObject();
+
+            spo.AttachTo(parent, true);
+
+            parent.ScaleX = .5f;
+            parent.ScaleY = .5f;
+            parent.ScaleZ = .5f;
+
+            spo.UpdateDependencies(0);
+
+
+            Assert.IsTrue(Math.Abs(spo.Position.X - 50f) < Single.Epsilon);
+            Assert.IsTrue(Math.Abs(spo.Position.Y - 100f) < Single.Epsilon);
+            Assert.IsTrue(Math.Abs(spo.Position.Z - 150f) < Single.Epsilon);
+        }
+
+        [TestMethod]
+        public void RelativeScalingDoesNotBreakPositioning()
+        {
+            var parent = new ScaledPositionedObject
+            {
+                Position = new Vector3
+                {
+                    X = 100f,
+                    Y = 200f,
+                    Z = 300f
+                },
+                ScaleX = .5f,
+                ScaleY = .5f,
+                ScaleZ = .5f
+            };
+
+            var spo = new ScaledPositionedObject();
+            spo.AttachTo(parent, true);
+            spo.RelativePosition = new Vector3(10f, 20f, 30f);
+
+            spo.UpdateDependencies(0);
+            
+            Assert.IsTrue(Math.Abs(spo.Position.X - 105f) < Single.Epsilon);      
+            Assert.IsTrue(Math.Abs(spo.Position.Y - 210f) < Single.Epsilon);
+            Assert.IsTrue(Math.Abs(spo.Position.Z - 315f) < Single.Epsilon);
+        }
+    }
+}
---
+++ spritertestgame/FlatRedBallExtensions/ScaledPositionedObject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlatRedBall;
+using FlatRedBall.Gui;
+using Microsoft.Xna.Framework;
+
+namespace FlatRedBallExtensions
+{
+    public class ScaledPositionedObject : PositionedObject
+    {
+        public float ScaleX { get; set; }
+        public float ScaleY { get; set; }
+        public float ScaleZ { get; set; }
+
+        public override void UpdateDependencies(double currentTime)
+        {
+// ReSharper disable once CompareOfFloatsByEqualityOperator
+            if (currentTime == LastDependencyUpdate) return;
+
+
+            // All parents up the chain need to be updated first
+            if (Parent != null)
+            {
+                Parent.UpdateDependencies(currentTime);
+            }
+
+            var scaledParent = Parent as ScaledPositionedObject;
+            if (scaledParent == null) return;
+
+            Position = new Vector3(RelativePosition.X*scaledParent.ScaleX,
+                RelativePosition.Y*scaledParent.ScaleY,
+                RelativePosition.Z*scaledParent.ScaleZ);
+            Position += Parent.Position;
+        }
+    }
+}
